25f504b8a038dedc6bcd4b6ad8309df9ed29b905
MeiyinPU <mei-yin.pu@oclock.school>
2024-10-16 23:13:14 +0200

    fix: payment page

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -64,7 +64,7 @@ function PaymentPage() {
             {/* 1st order */}
             <div className="border p-4">
               <div className="flex max-w-full flex-col">
-                <h3 className="border-b-4 border-b-secondary-200 py-6 pl-4 text-start text-3xl text-primary-950">
+                <h3 className="border-b-4 border-b-secondary-200 py-6 pl-4 text-start text-3xl text-secondary-500">
                   私人行程訂單
                 </h3>
 
@@ -97,7 +97,7 @@ function PaymentPage() {
 
             <div className="border p-4">
               <div className="flex max-w-full flex-col">
-                <h3 className="border-b-4 border-b-secondary-200 py-6 pl-4 text-start text-3xl text-primary-950">
+                <h3 className="border-b-4 border-b-secondary-200 py-6 pl-4 text-start text-3xl text-secondary-500">
                   定點行程訂單
                 </h3>
 
@@ -144,54 +144,112 @@ function PaymentPage() {
           </div>
 
           {/* right: Payment  */}
-          <div className="flex min-w-[46%] flex-col border p-4">
-            <h3 className="border-b-4 border-b-secondary-200 py-6 pl-4 text-start text-3xl text-primary-950">
-              私人行程訂單
-            </h3>
-            <div className="">
-              <div>
-                <div className="flex space-y-8 px-8 py-6">
-                  <div className="flex items-center justify-center space-x-4">
-                    <img
-                      src={CardData.img}
-                      alt=""
-                      className="inline-block h-20 max-w-20 rounded-full"
-                    />
-                    <p className="text-3xl text-secondary-700">
-                      {CardData.name}
-                    </p>
-                  </div>
-                </div>
-                <div className="flex justify-between">
-                  <p className="text-2xl text-grey-950">2024年10月19日</p>
-                  <p className="text-2xl">4位成人, 4位兒童</p>
-                  <p className="text-2xl text-primary-700">700 €</p>
-                </div>
+          <div className="flex min-w-[46%] flex-col border p-10">
+            <div className="space-y-4">
+              {/* Full Name */}
+              <div className="flex flex-col space-y-4">
+                <label
+                  htmlFor="full-name"
+                  className="text-3xl font-medium text-gray-700"
+                >
+                  訂購人姓名
+                </label>
+                <input
+                  type="text"
+                  id="full-name"
+                  placeholder="請輸入您的姓名"
+                  className="rounded-lg border border-gray-300 px-4 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary-500"
+                />
               </div>
 
-              {/* subtotal */}
-              <div className="self-end">
-                <div className="flex justify-between">
-                  <h4 className="text-3xl font-black text-primary-700">小計</h4>
-                  <p className="text-3xl font-black text-primary-700">700 €</p>
-                </div>
+              {/* Credit Card Number */}
+              <div className="flex flex-col space-y-4">
+                <label
+                  htmlFor="credit-card"
+                  className="text-3xl font-medium text-gray-700"
+                >
+                  信用卡帳號
+                </label>
+                <input
+                  type="text"
+                  id="credit-card"
+                  placeholder="xxxx-xxxx-xxxx-xxxx"
+                  className="rounded-lg border border-gray-300 px-4 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary-500"
+                />
+              </div>
+
+              {/* Phone Number */}
+              <div className="flex flex-col space-y-4">
+                <label
+                  htmlFor="phone"
+                  className="text-3xlfont-medium text-gray-700"
+                >
+                  電話
+                </label>
+                <input
+                  type="text"
+                  id="phone"
+                  placeholder="請輸入您的電話號碼"
+                  className="rounded-lg border border-gray-300 px-4 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary-500"
+                />
+              </div>
+
+              {/* Expiration Date */}
+              <div className="flex flex-col space-y-4">
+                <label
+                  htmlFor="expiry-date"
+                  className="text-3xl font-medium text-gray-700"
+                >
+                  有效日期
+                </label>
+                <input
+                  type="text"
+                  id="expiry-date"
+                  placeholder="MM/YY"
+                  className="rounded-lg border border-gray-300 px-4 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary-500"
+                />
+              </div>
+
+              {/* Email */}
+              <div className="flex flex-col space-y-4">
+                <label
+                  htmlFor="email"
+                  className="text-3xl font-medium text-gray-700"
+                >
+                  電子郵件信箱
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="請輸入您的電子郵件信箱"
+                  className="rounded-lg border border-gray-300 px-4 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary-500"
+                />
+              </div>
+
+              {/* Security Code */}
+              <div className="flex flex-col space-y-4">
+                <label
+                  htmlFor="security-code"
+                  className="text-3xl font-medium text-gray-700"
+                >
+                  安全碼
+                </label>
+                <input
+                  type="text"
+                  id="security-code"
+                  placeholder="CVV"
+                  className="rounded-lg border border-gray-300 px-4 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary-500"
+                />
               </div>
             </div>
 
-            <div className="flex flex-col">
-              <div className="my-20 items-center space-y-4">
+            <div className="m-auto">
+              <div className="my-20">
                 <button
-                  className="flex max-w-80 justify-center space-x-20 rounded-3xl border border-secondary-300 bg-secondary-400 px-4 py-4 text-white"
+                  className="flex max-w-80 justify-center space-x-20 rounded-3xl bg-primary-700 px-4 py-4 text-white"
                   // onClick={()=>handleConfirmOrderClick(id)}
                 >
-                  <p className="text-2xl">立刻預約</p>
-                </button>
-
-                <button
-                  className="flex max-w-80 justify-center space-x-20 rounded-3xl border border-secondary-600 bg-transparent px-4 py-4 text-secondary-600"
-                  // onClick={()=>handleGoBackClick(id)}
-                >
-                  <p className="text-2xl">回上一頁</p>
+                  <p className="text-2xl">送出資料</p>
                 </button>
               </div>
             </div>
